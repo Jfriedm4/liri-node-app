6dfc45b7d5acd517ded0a9d959f21fca5461453f
Jeremy Friedman <jeremyfriedman@Jeremys-MacBook-Air.local>
2018-02-07 17:32:56 -0500

    add features

--- functions.js
+++ functions.js
@@ -5,12 +5,16 @@
 //     var params = {screen_name: 'JeremyGWCoding'};
 //     client.get('statuses/user_timeline', params, function(error, tweets, response) {
 //         if (!error) {
-//             for (i = 0; i <4; i++) {
-//                 console.log("Tweet: " + tweets[i].text);
-//                 console.log("CREATED AT: " + tweets[i].created_at);
-//             }
-//         }
-//     })
+//             console.log("---------------------------TWEETS---------------------------------");
+//              for (i = 0; i <4; i++) {
+//                  console.log(
+//     `
+//     TWEET:
+//     ${tweets[i].text}
+//     CREATED AT:
+//     ${tweets[i].created_at}
+//     `);
+//              }
+//          }
+//      })
 // }
-
-// module.exports.getTweets = getTweets();
--- liri.js
+++ liri.js
@@ -1,6 +1,9 @@
-import { log } from "util";
-
+// user input variables
 var userCommand = process.argv;
+var songName = userCommand[3];
+var movieName = userCommand[3];
+var OMDBqueryURL = "";
+
 
 // Stuff being required
 require("dotenv").config();
@@ -8,59 +11,152 @@ var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require('request');
 var keys = require("./keys.js");
-var functions = require("./functions.js");
 
 // store the api keys in variables
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-// store functions in variables
-// var getTweets = functions.getTweets();
+// command functions
+function getTweets() {
+
+    var params = {screen_name: 'JeremyGWCoding'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log("---------------------------TWEETS---------------------------------");
+             for (i = 0; i <4; i++) {
+                 console.log(
+`
+TWEET:
+${tweets[i].text}
+CREATED AT:
+${tweets[i].created_at}
+`);
+             }
+         }
+     })
+}
+
+
+function getSong() {
+    if (songName) {
+        spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+        console.log("-------------------------SPOTIFY SEARCH---------------------------");
+        console.log("SONG TITLE: " + data.tracks.items[0].name);
+        console.log("ARTIST: " + data.tracks.items[0].album.artists[0].name); 
+        console.log("ALBUM TITLE: " + data.tracks.items[0].album.name);
+        console.log("PREVIEW LINK: " + data.tracks.items[0].album.external_urls.spotify);
+        });
+    } else {
+        console.log("I'M SORRY. PLEASE ENTER A SONG TITLE AFTER THE COMMAND 'spotify-this'.")
+    }
+}
+
+
+function getMovie() {
+    if (movieName) {
+        OMDBqueryURL = "https://www.omdbapi.com/?apikey=9f68b70&s&y=&plot=short&t=" + movieName;
+        request(OMDBqueryURL, function (error, response, body) {
+            //console.log('error:', error); // Print the error if one occurred
+            //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            var response = JSON.parse(body);
+            //console.log(response); // Print the HTML for the Google homepage.
+
+            console.log("-----------------------------OMDB SEARCH---------------------------");
+            console.log("TITLE: " + response.Title);
+            console.log("YEAR: " + response.Year);
+            console.log("IMDB RATING: " + response.Ratings[0].Value);
+            console.log("ROTTEN TOMATOES RATING: " + response.Ratings[1].Value);
+            console.log("COUNTRY: " + response.Country);
+            console.log("LANGUAGE: " + response.Language);
+            console.log("PLOT: " + response.Plot);
+            console.log("ACTORS: " + response.Actors);
+        });
+    } else {
+        OMDBqueryURL = "https://www.omdbapi.com/?apikey=9f68b70&s&y=&plot=short&t=rogue+one";
+        request(OMDBqueryURL, function (error, response, body) {
+            //console.log('error:', error); // Print the error if one occurred
+            //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            var response = JSON.parse(body);
+            //console.log(response); // Print the HTML for the Google homepage.
+
+            console.log("PLEASE ENTER A MOVIE TITLE. HERE'S AN EXAMPLE.")
+            console.log("-----------------------------OMDB SEARCH---------------------------");
+            console.log("TITLE: " + response.Title);
+            console.log("YEAR: " + response.Year);
+            console.log("IMDB RATING: " + response.Ratings[0].Value);
+            console.log("ROTTEN TOMATOES RATING: " + response.Ratings[1].Value);
+            console.log("COUNTRY: " + response.Country);
+            console.log("LANGUAGE: " + response.Language);
+            console.log("PLOT: " + response.Plot);
+            console.log("ACTORS: " + response.Actors);
+        });
+
+    }
+
+}
+
+
+function doThings() {
+    var fs = require("fs");
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(",");
+        userCommand[2] = dataArr[0];
+        songName = dataArr[1];
+        commands();
+    })
+
+}
+
+
+function commands() {
+    if (userCommand[2] === "twitters") {
+        getTweets();
+    
+    } else if (userCommand[2] === "spotify-this") {
+        getSong();
+    
+    } else if (userCommand[2] === "movie-this") {
+        getMovie();
+    
+    } else if (userCommand[2] === "do-things") {
+        doThings();
+    
+    } else if (userCommand[2]) {
+        console.log("I'M SORRY. THAT COMMAND IS NOT VALID.");
+    
+    } else {
+         console.log("PLEASE ENTER A COMMAND.");
+    }
+    }
 
 // on load user instructions
 console.log(`
 ------------------------------------------------------------------
                 /$$       /$$$$$$ /$$$$$$$  /$$$$$$
-               | $$      |_  $$_/| $$__  $$|_  $$_/
-               | $$        | $$  | $$  \ $$  | $$  
+               | $$      |_  $$_/| $$__ $$ |_  $$_/
+               | $$        | $$  | $$  \ $$   | $$  
                | $$        | $$  | $$$$$$$/  | $$  
-               | $$        | $$  | $$__  $$  | $$  
-               | $$        | $$  | $$  \ $$  | $$  
-               | $$$$$$$$ /$$$$$$| $$  | $$ /$$$$$$
+               | $$        | $$  | $$__ $$   | $$  
+               | $$        | $$  | $$  \ $$   | $$  
+               | $$$$$$$$ /$$$$$$| $$  |$$  /$$$$$$
                |________/|______/|__/  |__/|______/
 -----------------------------------------------------------------
 
 -----------------------------------------------------------------
-PLEASE ENTER A COMMAND IN THE FORMAT NODE LIRI 'COMMAND-NAME'
+PLEASE ENTER A COMMAND IN THE FORMAT: node liri 'COMMAND-NAME'
 -----------------------------------------------------------------
 
 VALID COMMANDS:
-* MY-TWEETS
-* SPOTIFY-THIS-SONG
-* MOVIE-THIS
-* DO-WHAT-IT-SAYS
+* twitters
+* spotify-this '<SONG NAME HERE>' or '<SONG NAME HERE & ARTIST NAME HERE>'
+* movie-this
+* do-things
 `);
 
-var params = {screen_name: 'JeremyGWCoding'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-         for (i = 0; i <4; i++) {
-             console.log("TWEET: " + tweets[i].text);
-             console.log("CREATED AT: " + tweets[i].created_at);
-         }
-     }
- })
-
-// user commands
-
-// if (userCommand[2] === "MY-TWEETS") {
-// }
-// } else if (userCommand[2] === "spotify-this-song") {
-
-// } else if (userCommand[2] === "movie-this") {
-
-// } else if (userCommand[2] === "do-what-it-says") {
-
-// } else {
-//     console.log("I'm sorry. This is not a valid command.")
-// }
+commands();
